08138cc53868cd139c8ff81cb3a50c52ff61d71f
Brian Hoffman <hoffmabc@users.noreply.github.com>
2024-09-23 12:42:10 -0400

    Catch backtrace

--- src/main.rs
+++ src/main.rs
@@ -29,23 +29,28 @@ async fn main() -> Result<()> {
     }
 
     // Match on the subcommand
-    match &cli.command {
-        Commands::Init => init().await?,
-        Commands::Server(ServerCommands::Start) => server_start(&config).await?,
-        Commands::Server(ServerCommands::Stop) => server_stop().await?,
-        Commands::Server(ServerCommands::Status) => server_status(&config).await?,
-        Commands::Server(ServerCommands::Logs { service }) => server_logs(service, &config).await?,
-        Commands::Deploy(args) => deploy(args, &config).await?,
-        Commands::Project(ProjectCommands::Clean) => clean().await?,
-        Commands::Dkg(DkgCommands::Start) => start_dkg(&config).await?,
-        Commands::Bitcoin(BitcoinCommands::SendCoins(args)) => send_coins(args, &config).await?,
-        Commands::Frontend(FrontendCommands::Start) => frontend_start().await?,
-        Commands::Account(AccountCommands::Create(args)) => create_account(args, &config).await?,
-        Commands::Config(ConfigCommands::View) => config_view(&config).await?,
-        Commands::Config(ConfigCommands::Edit) => config_edit(&config).await?,
-        Commands::Config(ConfigCommands::Reset) => config_reset().await?,
-        Commands::Start => server_start(&config).await?,
-        Commands::Stop => server_stop().await?,
+    let result = match &cli.command {
+        Commands::Init => init().await,
+        Commands::Server(ServerCommands::Start) => server_start(&config).await,
+        Commands::Server(ServerCommands::Stop) => server_stop().await,
+        Commands::Server(ServerCommands::Status) => server_status(&config).await,
+        Commands::Server(ServerCommands::Logs { service }) => server_logs(service, &config).await,
+        Commands::Deploy(args) => deploy(args, &config).await,
+        Commands::Project(ProjectCommands::Clean) => clean().await,
+        Commands::Dkg(DkgCommands::Start) => start_dkg(&config).await,
+        Commands::Bitcoin(BitcoinCommands::SendCoins(args)) => send_coins(args, &config).await,
+        Commands::Frontend(FrontendCommands::Start) => frontend_start().await,
+        Commands::Account(AccountCommands::Create(args)) => create_account(args, &config).await,
+        Commands::Config(ConfigCommands::View) => config_view(&config).await,
+        Commands::Config(ConfigCommands::Edit) => config_edit(&config).await,
+        Commands::Config(ConfigCommands::Reset) => config_reset().await,
+        Commands::Start => server_start(&config).await,
+        Commands::Stop => server_stop().await,
+    };
+
+    if let Err(e) = result {
+        println!("Error: {}", e);
+        std::process::exit(1);
     }
 
     Ok(())
